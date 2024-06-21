b9fd56a667c5820b5ed193dd213cb880633783ca
Nika-Chinchaladze <nika2019chinchaladze@gmail.com>
2024-06-21 11:16:13 +0400

    removeBook function has been tested completely

--- cypress/e2e/removeBook.cy.js
+++ cypress/e2e/removeBook.cy.js
@@ -25,6 +25,17 @@ describe("removeBook()", () => {
             expect(result.response.body.message).to.eq("Book has been deleted successfully!");
         })
         cy.get('[data-cy="book-content"] li').should("exist").should("have.length", 2);
-
+    });
+    
+    it("should console log error message if network error occurs", () => {
+        // Arrange & Mocking
+        cy.intercept("GET", "http://localhost:5000/books", { fixture: "books.json" }).as("getBooks");
+        cy.visit("/");
+        cy.intercept("DELETE", "http://localhost:5000/books/3", { forceNetworkError: true }).as("deleteBook");
+        // Act
+        cy.get('[data-cy="book-content"] li').last().as("lastBook");
+        cy.get("@lastBook").find(".delete-btn").click();
+        // Assert
+        cy.wait("@deleteBook").its("error").should("not.be.undefined");
     });
 });
